b3d2d6e66ff1e56d6a66c4f7b4082c41a5367d80
moznion <moznion@gmail.com>
2015-12-07 23:59:56 +0900

    Cmd

--- src/main/java/net/moznion/memai/memcached/Client.java
+++ src/main/java/net/moznion/memai/memcached/Client.java
@@ -1,8 +1,12 @@
 package net.moznion.memai.memcached;
 
+import net.moznion.memai.memcached.command.DecrementCommand;
 import net.moznion.memai.memcached.command.GetCommand;
 import net.moznion.memai.memcached.command.GetsCommand;
+import net.moznion.memai.memcached.command.IncrementCommand;
 import net.moznion.memai.memcached.command.SetCommand;
+import net.moznion.memai.memcached.protocol.text.request.incremental.TextDecrementProtocol;
+import net.moznion.memai.memcached.protocol.text.request.incremental.TextIncrementProtocol;
 import net.moznion.memai.memcached.protocol.text.request.retrieval.TextGetProtocol;
 import net.moznion.memai.memcached.protocol.text.request.retrieval.TextGetsProtocol;
 import net.moznion.memai.memcached.protocol.text.request.storage.TextSetProtocol;
@@ -64,6 +68,16 @@ public class Client {
     public GetsCommand gets(final List<String> keys) {
         balance();
         return new GetsCommand(new TextGetsProtocol(keys), workers.get(cursor));
+    }
+
+    public IncrementCommand incr(final String key, final long value) {
+        balance();
+        return new IncrementCommand(new TextIncrementProtocol(key, value), workers.get(cursor));
+    }
+
+    public DecrementCommand decr(final String key, final long value) {
+        balance();
+        return new DecrementCommand(new TextDecrementProtocol(key, value), workers.get(cursor));
     }
 
     public void shutdown() {
